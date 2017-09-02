36c2307588e81b1a3cea49b6d14fd200dcf37114
Dmitry Sablin <91dims@gmail.com>
2017-09-02 23:07:43 +0500

    finish module1-solution

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -5,6 +5,37 @@
 
   .controller("LunchCheckController", LunchCheckController);
 
-  function LunchCheckController () {
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+
+    $scope.items = ""
+
+    $scope.checkItems = function (){
+      var itemCount = countItems($scope.items);
+      displayMessage(itemCount);
+    };
+
+    function displayMessage(itemCount){
+      var message;
+      var validationState = "has-success";
+      if (itemCount == 0){
+        message = "Please enter data first";
+        validationState = "has-error"
+      } else if (itemCount <= 3) {
+        message = "Enjoy!";
+      } else {
+        message = "Too much!";
+      }
+      $scope.message = message;
+      $scope.validationState = validationState;
+    };
+
+    function countItems(items){
+      var re = /\s*,\s*/;
+      // split and filter all "" elements
+      // "" filtered because "" is falsy
+      var itemCount = items.split(re).filter(function(i){return i}).length;
+      return itemCount;
+    };
   };
 })();
