fd233aedf80d82f08eaeda5872438fc3f96e35b8
Dmitry Sablin <91dims@gmail.com>
2017-09-04 17:04:19 +0500

    init module2-solution

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # angular-exercises
 
 [module1-solution (Check Lunch item count)](module1-solution/index.html)
+
+[module2-solution (Two List App)](module2-solution/index.html)
---
+++ module2-solution/js/app.js
@@ -0,0 +1,11 @@
+(function (){
+  'use strict';
+
+  angular.module("ShoppingListCheckOff", [])
+  .controller("Controller", Controller);
+
+  Controller.$inject = ['$scope'];
+  function Controller($scope) {
+
+  };
+})();
